343011961ab48516f18f8d34995651348be31fec
bmwoolf <bradleymwoolf@gmail.com>
2023-09-14 23:10:02 -0700

    post dated check

---
+++ contracts/post_order_check.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0
+
+contract PostDatedCheck {
+    
+    // payment details
+    struct Payment {
+        address payable recipient;
+        uint256 amount;
+        uint256 data;
+        bool executed;
+    }
+
+    // uint256 pulib paymentCount;
+    mapping(uint256 => Payment) public payments;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+    }
+
+    // create payment based on date
+    function createPayment(address payable, _recipient, uint256, _amount,  uint256 _date) public onlyOwner {
+        payments[paymentCount] = Payment({
+            recipient: _recipeient,
+            amount: _amount,
+            date: _date, 
+            executed: false
+        })
+
+        paymentCount += 1;
+    }
+
+    // execute the payment
+    function executePayment(uint256, _id) public {
+        Payment storage payment = payments[_id];
+
+        require(address(this).balance >= payment.amount, "Insufficient balance");
+        require(block.timestamp >= payment.date, "Date not reached");
+        require(!payment.executed, "Payment already executed");
+
+        payment.recipient.transfer(payment.amount);
+        payment.executed = true;
+    }
+
+    function deposit() public payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
